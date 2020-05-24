76d70830b32e37390017caeac81b7f6063dba568
SnowzTail <i@snowztail.com>
2020-05-24 17:34:54 +0000

    :lipstick: Cache dark mode to local storage

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,22 +3,34 @@ var _Blog = _Blog || {};
 
 // Dark Mode
 _Blog.switchDarkMode = function () {
-    const currentTheme = document.cookie.replace(/(?:(?:^|.*;\s*)dark\s*\=\s*([^;]*).*$)|^.*$/, "$1") || '0';
-    const isDark = currentTheme === '1';
-    document.body.classList.toggle('dark-theme', isDark);
-    // 手动切换 Dark Mode
-    const themeSwitcher = document.querySelectorAll('.theme-switch');
-    themeSwitcher.forEach(function (themeSwitcherItem) {
-        themeSwitcherItem.addEventListener('click', () => {
-            const currentTheme = document.cookie.replace(/(?:(?:^|.*;\s*)dark\s*\=\s*([^;]*).*$)|^.*$/, "$1") || '0';
-            if (currentTheme == '0') {
-                document.body.classList.add('dark-theme');
-                document.cookie = "dark=1;path=/";
-                console.log('Dark mode on');
+    let darkMode = localStorage.getItem("darkMode");
+    // console.log(darkMode);
+    const darkModeToggle = document.querySelectorAll(".theme-switch");
+
+    darkModeToggle.forEach(function (darkModeToggleItem) {
+
+        const enableDarkMode = () => {
+            document.body.classList.add('dark-theme');
+            localStorage.setItem("darkMode", "enabled");
+        }
+
+        const disableDarkMode = () => {
+            document.body.classList.remove("dark-theme");
+            localStorage.setItem("darkMode", "disabled");
+        }
+
+        if (darkMode === "enabled") {
+            enableDarkMode();
+        }
+
+        darkModeToggleItem.addEventListener('click', () => {
+            darkMode = localStorage.getItem('darkMode');
+            if (darkMode != "enabled") {
+                enableDarkMode();
+                // console.log(darkMode);
             } else {
-                document.body.classList.remove('dark-theme');
-                document.cookie = "dark=0;path=/";
-                console.log('Dark mode off');
+                disableDarkMode();
+                // console.log(darkMode);
             }
         });
     });
@@ -53,17 +65,6 @@ _Blog.scrollIndicator = function () {
         });
     });
 }
-
-// // 在用户切换网页时改变浏览器标题
-// _Blog.changeTile = function() {
-//     let currentTile = document.title;
-//     window.onblur = function() {
-//         this.document.title = '别走啊，官人 _(:з」∠)_';
-//     }
-//     window.onfocus = function() {
-//         this.document.title = currentTile;
-//     }
-// }
 
 // 为代码块添加 Copy 按钮
 _Blog.addCopyBottons = function () {
@@ -119,5 +120,4 @@ document.addEventListener('DOMContentLoaded', function () {
     _Blog.switchDarkMode();
     _Blog.switchMobileMenu();
     _Blog.scrollIndicator();
-    // _Blog.changeTile();
 });
